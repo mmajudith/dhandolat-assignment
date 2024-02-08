f52ab461255fe44f85e565dc356829c5fef0adaf
mmajudith <mmajudith2000@gmail.com>
2024-02-08 11:31:07 -0800

    feat
    Added toggle menu feature

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import NavList from './NavList/NavList';
+import { ReactComponent as HamMenuIcon } from '../../assets/hamburger-menu.svg';
 import './header.css';
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleIsOpen = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<header className="w-90 m-auto flex-row between">
-			<div className="logo">LOGO</div>
+			<div className="flex-row center logo-menu-wrapper">
+				<div className="logo">LOGO</div>
 
+				<div onClick={handleIsOpen} className="menu-icon-wrapper">
+					<HamMenuIcon />
+				</div>
+			</div>
+
+			{/* {isOpen } */}
 			<NavList />
 		</header>
 	);
